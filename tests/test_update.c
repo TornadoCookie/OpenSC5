cf85f8ee346bd57e3da2725cb17b46f6fb920ef5
Helix Graziani <helix.graziani@hotmail.com>
2024-06-19 21:01:49 -0400

    Updating works.

--- tests/test_update.c
+++ tests/test_update.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <raylib.h>
 
 struct MemoryStruct {
   char *memory;
@@ -29,6 +30,26 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
   return realsize;
 }
 
+#define strlitsize(s) (sizeof(s)-1)
+
+void download_to_file(const char *url, FILE *f)
+{
+    CURL *curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "Simcity-Game/10.3.4.0");
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
+
+    CURLcode code = curl_easy_perform(curl);
+
+    if (code != CURLE_OK)
+    {
+        printf("Failed: %s\n", curl_easy_strerror(code));
+    }
+
+    curl_easy_cleanup(curl);
+}
+
 int main()
 {
     struct MemoryStruct chunk;
@@ -54,6 +75,32 @@ int main()
     else
     {
         printf("Retrieved %lu bytes.\n", chunk.size);
+        
+
+        char *cursor = chunk.memory;
+        while ((cursor - chunk.memory) < chunk.size)
+        {
+            cursor = strchr(cursor, '\"')+1;
+            if ((cursor - chunk.memory) > chunk.size) break;
+            char *end = strchr(cursor, '\"');
+            int length = end - cursor;
+            char *packagefilename = malloc(length+1);
+            memcpy(packagefilename, cursor, length);
+            packagefilename[length] = 0;
+            printf("%s\n", packagefilename);
+
+            const char *path = TextFormat("update/%s", packagefilename);
+
+            if (!FileExists(path))
+            {
+                FILE *f = fopen(path, "w+");
+                download_to_file(TextFormat("http://update.prod.simcity.com/%s", packagefilename), f);
+                fclose(f);
+            }
+
+            cursor += strlitsize("\">");
+            cursor += length;
+        }
     }
 
     curl_easy_cleanup(curl);
