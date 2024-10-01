903095070bca8ef2976a8fb0532dca83d10432f4
Helix Graziani <helix.graziani@hotmail.com>
2024-10-01 19:32:08 -0400

    more statefile things

--- src/filetypes/statefile.c
+++ src/filetypes/statefile.c
@@ -10,14 +10,38 @@ typedef struct StateFileChunk {
     uint32_t size;
 } StateFileChunk;
 
+typedef struct StateFileHeader0 {
+    uint32_t unknown[3];
+    uint32_t unknown2Count;
+} StateFileHeader0;
+
+typedef struct StateFileHeader {
+    uint32_t unknown3[6];
+    uint32_t imageCount;
+} StateFileHeader;
+
 StateFile LoadStateFile(const char *filename)
 {
     FILE *f = fopen(filename, "rb");
 
-    fseek(f, 0x2C, SEEK_SET); // skip the header, it is giving us no useful info at the moment.
+    StateFileHeader0 header0 = { 0 };
+    
+    fread(&header0, sizeof(StateFileHeader0), 1, f);
+
+    if (header0.unknown2Count)
+    {
+        header0.unknown2Count = be32toh(header0.unknown2Count);
+        printf("unknown 2 count: %d\n", header0.unknown2Count);
+        fseek(f, 2 * header0.unknown2Count * 4, SEEK_CUR);  
+    }
+
+    StateFileHeader header = { 0 };
+    fread(&header, sizeof(StateFileHeader), 1, f);
+
+    header.imageCount = be32toh(header.imageCount);
 
     // TODO this assumes the file is already gzip decompressed.
-    while (!feof(f))
+    for (int i = 0; i < header.imageCount; i++)
     {
         StateFileChunk chunk = { 0 };
 
@@ -26,18 +50,18 @@ StateFile LoadStateFile(const char *filename)
         //if (chunk.null != 0) printf("expected NULL, got %#x\n", chunk.null);
         printf("signature: %#lx\n", chunk.signature);
 
-        chunk.size = htobe32(chunk.size);
+        chunk.size = be32toh(chunk.size);
 
         printf("size: %d\n", chunk.size);
         
         uint32_t *imgData = malloc(chunk.size * 4);
         fread(imgData, 4, chunk.size, f);
 
-        for (int i = 0; i < chunk.size; i++)
+        for (int j = 0; j < chunk.size; j++)
         {
-            Color *col = ((Color*)imgData)+i;
-            uint32_t asint = imgData[i];
-            asint = htobe32(asint);
+            Color *col = ((Color*)imgData)+j;
+            uint32_t asint = imgData[j];
+            asint = be32toh(asint);
             uint16_t as16 = asint & 0xFFFF;
             float asflt = as16;
             asflt /= 0xFFFF;
@@ -59,5 +83,16 @@ StateFile LoadStateFile(const char *filename)
         ExportImage(img, TextFormat("corrupted/%#lx.png", chunk.signature));
     }
 
+    long current = ftell(f);
+    printf("Ended at address %#lx.\n", current);
+
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    fseek(f, current, SEEK_SET);
+
+    void *lzCompData = malloc(size - current);
+    fread(lzCompData, 1, size - current, f);
+
+    //SaveFileData("corrupted/extra.Z", lzCompData, size - current);
 
 }
