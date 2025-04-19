91cf8b0b168b53588c06c231eefb7f71c2ef4e3d
Helix Graziani <helix.graziani@hotmail.com>
2025-04-19 19:45:43 -0400

    make the simcity exe launch Activation.dll

--- activation/src/activation.c
+++ activation/src/activation.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define DllImport   __declspec( dllimport )
+#define DllExport   __declspec( dllexport )
 
 /* chat HELP */
-void Launch(void)
+DllExport void LaunchActivation(void)
 {
+    printf("LONG LIVE SIMCITY, hello from Core/Activation.dll\n");
+    exit(EXIT_SUCCESS);
 }
 
